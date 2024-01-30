b4e1f75d94b59546992f0af4c0b13d730ef05ef0
AverageHelper <me@average.name>
2024-01-30 13:29:49 -0700

    feat: Add Links page

---
+++ src/helpers/checkUrl.ts
@@ -0,0 +1,62 @@
+/* eslint-disable no-console */
+
+import { FgBlue, FgCyan, FgGreen, FgRed, Reset } from "./consoleColors.js";
+
+const alreadyOk = new Set<string>();
+
+/**
+ * Resolves if the given URL is accessible. Throws a {@link TypeError} otherwise.
+ *
+ * Only runs when the `CI` environment variable is set to `true`.
+ *
+ * @param src The URL to check.
+ */
+export async function checkUrl(src: URL): Promise<void> {
+	if (import.meta.env["CI"] !== "true") {
+		// Only run in CI environment
+		return;
+	}
+
+	// Check cache, make sure we haven't already checked this URL
+	if (alreadyOk.has(src.href)) {
+		console.info(`href ${FgBlue}'${src.href}'${FgGreen} OK (skipped)${Reset}`);
+		return;
+	}
+
+	try {
+		// Try fetch with HEAD method
+		const result = await fetch(src, { method: "HEAD" });
+		let didRedirect = result.redirected;
+		if (result.status === 405) {
+			// HEAD isn't allowed for some reason. Try GET
+			const result2 = await fetch(src, { method: "GET" });
+			didRedirect = result2.redirected;
+			if (!result2.ok) throw result2.status;
+		} else if (!result.ok) {
+			throw result.status;
+		}
+
+		// Success! Remember this URL for later
+		alreadyOk.add(src.href);
+		if (didRedirect) {
+			console.info(`href ${FgBlue}'${src.href}'${FgCyan} OK (redirected)${Reset}`);
+		} else {
+			console.info(`href ${FgBlue}'${src.href}'${FgGreen} OK${Reset}`);
+		}
+	} catch (error) {
+		if (typeof error === "number") {
+			// Got non-OK HTTP response
+			console.info(`href ${FgBlue}'${src.href}'${FgRed} HTTP ${error}${Reset}`);
+			throw new TypeError(`${FgRed}Link href '${src.href}' is broken: ${error}${Reset}`);
+		} else if (error instanceof Error) {
+			// Got network error
+			console.info(`href ${FgBlue}'${src.href}'${FgRed} Network Error: ${error.message}${Reset}`);
+			throw new TypeError(`${FgRed}Link href '${src.href}' is broken: ${error.message}${Reset}`);
+		} else {
+			// Got unknown error
+			const message = JSON.stringify(error);
+			console.info(`href ${FgBlue}'${src.href}'${FgRed} Unknown Error: ${message}${Reset}`);
+			throw new TypeError(`${FgRed}Link href '${src.href}' is broken: ${message}${Reset}`);
+		}
+	}
+}
---
+++ src/helpers/consoleColors.ts
@@ -0,0 +1,29 @@
+// See https://stackoverflow.com/a/41407246
+
+export const Reset = "\u001B[0m";
+export const Bright = "\u001B[1m";
+export const Dim = "\u001B[2m";
+export const Underscore = "\u001B[4m";
+export const Blink = "\u001B[5m";
+export const Reverse = "\u001B[7m";
+export const Hidden = "\u001B[8m";
+
+export const FgBlack = "\u001B[30m";
+export const FgRed = "\u001B[31m";
+export const FgGreen = "\u001B[32m";
+export const FgYellow = "\u001B[33m";
+export const FgBlue = "\u001B[34m";
+export const FgMagenta = "\u001B[35m";
+export const FgCyan = "\u001B[36m";
+export const FgWhite = "\u001B[37m";
+export const FgGray = "\u001B[90m";
+
+export const BgBlack = "\u001B[40m";
+export const BgRed = "\u001B[41m";
+export const BgGreen = "\u001B[42m";
+export const BgYellow = "\u001B[43m";
+export const BgBlue = "\u001B[44m";
+export const BgMagenta = "\u001B[45m";
+export const BgCyan = "\u001B[46m";
+export const BgWhite = "\u001B[47m";
+export const BgGray = "\u001B[100m";
